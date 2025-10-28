77583bcbbd1759058536d6235b5a348485cf0e29
nlisa92 <irina.zamula.92@gmail.com>
2025-10-28 22:30:08 +0300

    Обновила адрес homepage и настройки деплоя

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todo-redux/",
+  base: "/todo-api-rtk/",
   build: {
     chunkSizeWarningLimit: 1000,
   },
